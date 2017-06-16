8dec50d843bf30616075abb47aef82364cd5f665
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 21:13:02 +0100

    Update project configuration

--- project.clj
+++ project.clj
@@ -1,10 +1,20 @@
 (defproject com.ninjakoala/monotony "1.2.0-SNAPSHOT"
   :description "Keep things flat. Redirects timbre to tools.logging"
-  :license {:name "Eclipse Public License - v 1.0"
+  :url "https://github.com/ninjakoala/monotony"
+  :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.taoensso/timbre "3.1.6"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.slf4j/slf4j-api "1.7.7"]]
-  :plugins [[lein-release "1.0.5"]]
-  :lein-release {:deploy-via :clojars})
+  :dependencies [[com.taoensso/timbre "4.10.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.slf4j/slf4j-api "1.7.25"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]]}}
+  :aliases {"test" ["midje"]}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
